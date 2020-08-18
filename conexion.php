ec73a75c66c970e9acddd7b6d3f1b6dc0a0717bf
Your Name <you@example.com>
2020-08-18 16:46:43 -0300

    se corrige conexion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     
   // Variables de la conexion a la DB
-     $mysqli = new mysqli("localhost","root","","asignaciones"); //localhost
+     $mysqli = new mysqli("lubnenikhwlwdz","5d2aa437c098caaf91bc4a0f3b31bce389848f72d82c74aa987b13eb010ba773","","df1k34d9omoppf"); //localhost
   // Comprobamos la conexion
     if($mysqli->connect_errno) {
         die("Fallo la conexion");
