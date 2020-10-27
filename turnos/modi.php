5af83df2573ce2bf0dcff3cde3fe0cca14de472e
Your Name <you@example.com>
2020-10-27 17:31:02 -0300

    se corrigen errores en turnos de botones atras

--- turnos/modi.php
+++ turnos/modi.php
@@ -39,7 +39,7 @@ $mysqli->close();
 
 ?>
 
-<a href="../turnos">	 <hr class="mb-4">
+<a href="javascript:history.go(-1)">	 <hr class="mb-4">
 <DIV ALIGN=center>
         <button class="btn btn-success btn-lg btn-block col-sm-5" type="submit">Volver</button>
         </div>
--- turnos/modifica.php
+++ turnos/modifica.php
@@ -14,7 +14,7 @@
 <body background="../img/turnos2.jpg" style="background-size:cover";>
   <div class="col-md"><h2>
 		<img border="0" src="../img/atras.png" width="42" height="42" title="Volver al menú"
-    onclick=location.href='../turnos/'; onKeyPress=location.href='../turnos/'; style='cursor:pointer';/>
+    onclick=location.href='javascript:history.go(-1)'; onKeyPress=location.href='javascript:history.go(-1)'; style='cursor:pointer';/>
     <label for="boton_atras"><h6>Atrás</h6></label>
 	</h2></div>
 
